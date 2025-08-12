9a04674c76c75a1b2acab0513c0eaa92412090f6
Marcos <marcos.chiu@gmail.com>
2025-08-12 19:55:56 -0300

    refactoring init

--- scripts/tea_pot.gd
+++ scripts/tea_pot.gd
@@ -2,30 +2,36 @@ extends MeshInstance3D
 
 signal piece_solved
 
-const TOLERANCE: float = 0.2
+const TOLERANCE: float = 0.05
 var solved: bool = false
 
-const SOLUTION := Vector3(1.0, -179.0, 0)
+const SOLUTION := Vector3(0.0, 3.12, 0.0)
 
 func _ready() -> void:
 	rotation_degrees = Vector3.ZERO
-#	position = Vector3.ZERO
 
 func _input(_event: InputEvent) -> void:
 	if is_solved():
 		piece_solved.emit()
 		return
 
-func angle_diff_deg(a: float, b: float) -> float:
+func _angle_diff_deg(a: float, b: float) -> float:
 	var diff = fposmod(a - b, 360.0)
 	if diff > 180.0:
 		diff -= 360.0
 	return diff
 
+func check_piece_solution() -> void:
+	if is_solved():
+		print("yes, my rotation is ", rotation)
+	else:
+		print("no, I'm currently at", rotation)
+	print("my position is ", position)
+
 func is_solved() -> bool:
-	var dx = angle_diff_deg(rotation_degrees.x, SOLUTION.x)
-	var dy = angle_diff_deg(rotation_degrees.y, SOLUTION.y)
-	var dz = angle_diff_deg(rotation_degrees.z, SOLUTION.z)
+	var dx: float = _angle_diff_deg(rotation.x, SOLUTION.x)
+	var dy: float = _angle_diff_deg(rotation.y, SOLUTION.y)
+	var dz: float = _angle_diff_deg(rotation.z, SOLUTION.z)
 
 	return abs(dx) <= TOLERANCE \
 		and abs(dy) <= TOLERANCE \
