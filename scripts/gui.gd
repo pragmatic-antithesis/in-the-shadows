5560c275e4a67804b4fc1cc1c0ab3b229004d416
Marcos <marcos.chiu@gmail.com>
2025-08-18 19:54:08 -0300

    shadows

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -1,25 +1,24 @@
 extends Control
 
-@onready var menu_background: Material = $Canvas/AlphaTint.material;
-@onready var title: RichTextLabel = $GameTitle
-# tweak these numbers to taste
-const MAX_OFFSET := 24.0
+@onready var menu_background: Material = $MenuCanvas/AlphaTint.material;
+@onready var title: RichTextLabel = $TitleCanvas/GameTitle
+
+const MAX_OFFSET := 18.0
 
 func _ready() -> void:
-	var button: MenuButton = $Canvas/Menu/MenuButton
+	var button: MenuButton = $MenuCanvas/Menu/MenuButton
 	button.pressed.connect(_on_start_button_pressed);
 	menu_background.set_shader_parameter("aspect_ratio", size.y / size.x)
 
 func _on_start_button_pressed() -> void:
-	var basecolor = menu_background.get_shader_parameter("base_color")
-	print(basecolor)
-	menu_background.set_shader_parameter("base_color", Vector4(0.0, 0.0, 0.0, 0.0))
+	for child in get_children():
+		child.hide()
 	hide()
 
-func _on_color_rect_gui_input(event: InputEvent) -> void:
+func _on_alpha_tint_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var uv_mouse: Vector2 = get_local_mouse_position() / size
 		menu_background.set_shader_parameter("u_mouse", uv_mouse)
 		var dir: Vector2 = (uv_mouse - Vector2(0.5, 0.5)) * 2.0
-		title.add_theme_constant_override("shadow_offset_x", dir.x * MAX_OFFSET)
-		title.add_theme_constant_override("shadow_offset_y", dir.y * MAX_OFFSET)
+		title.add_theme_constant_override("shadow_offset_x", int(dir.x * MAX_OFFSET))
+		title.add_theme_constant_override("shadow_offset_y", int(dir.y * MAX_OFFSET))
