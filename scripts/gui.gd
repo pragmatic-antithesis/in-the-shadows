ca3ad9de134575e2314e4dd7a99fc7e54043af29
Marcos <marcos.chiu@gmail.com>
2025-08-18 11:50:48 -0300

    changed layout and organization

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -1,17 +1,19 @@
 extends Control
 
+@onready var menu_background: Material = $ColorRect.material;
 
 func _ready() -> void:
 	var button: MenuButton = $Menu/MenuButton
 	button.pressed.connect(_on_start_button_pressed);
-	$ColorRect.material.set_shader_parameter("aspect_ratio", size.y / size.x)
+	menu_background.set_shader_parameter("aspect_ratio", size.y / size.x)
 
 func _on_start_button_pressed() -> void:
-	var basecolor = $ColorRect.material.get_shader_parameter("base_color")
+	var basecolor = menu_background.get_shader_parameter("base_color")
 	print(basecolor)
-	$ColorRect.material.set_shader_parameter("base_color", Vector4(0.0, 0.0, 0.0, 0.0))
+	menu_background.set_shader_parameter("base_color", Vector4(0.0, 0.0, 0.0, 0.0))
+	hide()
 
 func _on_color_rect_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var uv_mouse: Vector2 = get_local_mouse_position() / size
-		$ColorRect.material.set_shader_parameter("u_mouse", uv_mouse)
+		menu_background.set_shader_parameter("u_mouse", uv_mouse)
