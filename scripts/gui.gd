e7f00b51a0c7bb469560ffee8646dbb4a6205c91
Marcos <marcos.chiu@gmail.com>
2025-08-18 12:57:38 -0300

    shadowing title

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -1,9 +1,13 @@
 extends Control
 
-@onready var menu_background: Material = $ColorRect.material;
+@onready var menu_background: Material = $Canvas/AlphaTint.material;
+@onready var title: RichTextLabel = $GameTitle
+# tweak these numbers to taste
+const MAX_OFFSET := 12.0
+const MAX_DARKEN := 0.6
 
 func _ready() -> void:
-	var button: MenuButton = $Menu/MenuButton
+	var button: MenuButton = $Canvas/Menu/MenuButton
 	button.pressed.connect(_on_start_button_pressed);
 	menu_background.set_shader_parameter("aspect_ratio", size.y / size.x)
 
@@ -17,3 +21,14 @@ func _on_color_rect_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var uv_mouse: Vector2 = get_local_mouse_position() / size
 		menu_background.set_shader_parameter("u_mouse", uv_mouse)
+		# make it relative to center
+		var dir = (uv_mouse - Vector2(0.5, 0.5)) * 2.0  # (-1..1)
+
+		# affect drop shadow
+		title.add_theme_constant_override("shadow_offset_x", dir.x * MAX_OFFSET)
+		title.add_theme_constant_override("shadow_offset_y", dir.y * MAX_OFFSET)
+
+		# fade text (fake darkening when “light” is far)
+		var s = clamp(dir.length(), 0.0, 1.0)
+		var m = 1.0 - s * MAX_DARKEN
+		title.modulate = Color(m, m, m, 1.0)
