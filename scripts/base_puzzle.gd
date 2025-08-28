a271431cfbe2bf021ebe4b553006c2004215e33b
Marcos <marcos.chiu@gmail.com>
2025-08-28 15:52:37 -0300

    textures

--- scripts/base_puzzle.gd
+++ scripts/base_puzzle.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 signal puzzle_solved
 
+@export var level_id: int
 @onready var meshes: Array[MeshInstance3D]
 @onready var children: Array[Node]
 var completion_status: Array[bool] = []
@@ -19,13 +20,9 @@ func _ready() -> void:
 			children[i].piece_solved.connect(_on_child_solved.bind(i))
 		else:
 			push_warning("Child %s does not have piece_solved signal" % meshes[i].name)
-
-	#if children.size() > 1:
-		#children[1].control_click = MOUSE_BUTTON_RIGHT
+	level_id = meshes[0].level_id
 
 func _on_child_solved(child_index: int) -> void:
 	completion_status[child_index] = true
-	#if children.size() > 1:
-		#if !child_index: children[1].control_click = MOUSE_BUTTON_LEFT
 	if completion_status.all(func(status): return status):
-		puzzle_solved.emit(meshes[0].level_id)
+		puzzle_solved.emit(level_id)
